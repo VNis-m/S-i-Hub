31598fa4e76195207692e31f52bad4fcdf5f1cd1
VNis-m <lephuocquang2k8@gmail.com>
2024-12-30 20:20:56 +0700

    Create DinoSukuna.lua

---
+++ DinoSukuna.lua
@@ -0,0 +1,7 @@
+
+Config = {
+    namescript = "Sói Hub",--Tên Script
+    logoscript = "107095095031813",--ID Logo Script
+    tacgia = "DinoSukuna"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))()
